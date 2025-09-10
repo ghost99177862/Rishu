ec99e4807b3a3c7a078233e8b93f13d5d724a0e1
Virus <virus2517a@gmail.com>
2025-09-10 19:54:57 +0530

    new

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app = require('./app');
+const http = require('http');
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
